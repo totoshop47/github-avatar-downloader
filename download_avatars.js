fc1122204c24a90c0b2040971506b0e6966f250b
Jimyung Park <pzy51992@hotmail.com>
2018-12-04 03:46:34 +0000

    make dynamic values of url and filePath, passing them thru the function downloadImageByURL to download pics

--- download_avatars.js
+++ download_avatars.js
@@ -21,21 +21,29 @@ function getRepoContributors(repoOwner, repoName, cb) {
 }
 
 function downloadImageByURL(url, filePath) {
-  // download pic from given url with filepath
   request.get(url)
-
-  .pipe(fs.createWriteStream(filePath));
+  .pipe(fs.createWriteStream(filePath));  // downloading pics from avatar_urls with filename with login values
 }
 
 getRepoContributors("jquery", "jquery", function(err, result) {
   console.log("Errors:", err);
   result.forEach(function(obj) {
-    console.log(obj.avatar_url); // printing out avatar_urls
-
-  })
+    var url = obj.avatar_url; // setting up avatar_urls to variable name url
+    var filePath = obj.login; // setting up login values to variabale name filePath
+    downloadImageByURL(url, './avatars/'+filePath+'.jpg');
+  });
   // console.log("Result:", result);
 });
 
-downloadImageByURL("https://avatars2.githubusercontent.com/u/2741?v=3&s=466", "avatars/kvirani.jpg")
+// getRepoContributors("jquery", "jquery", function(err, result) {
+//   console.log("Errors:", err);
+//   result.forEach(function(obj) {
+//     console.log(obj.avatar_url); // printing out avatar_urls
+//     console.log(obj.login); // printing out login values
+//   })
+//   // console.log("Result:", result);
+// });
+
+
 
 
