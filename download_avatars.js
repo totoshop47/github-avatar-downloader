84ebb29d5f5e2ba05765d3064fb727c19731ab89
Jimyung Park <pzy51992@hotmail.com>
2018-12-04 01:40:12 +0000

    authorization header added

--- download_avatars.js
+++ download_avatars.js
@@ -1,7 +1,21 @@
 var request = require('request');
+var token = require('./secrets.js')
+
+console.log('Welcome to the GitHub Avatar Downloader!');
 
 function getRepoContributors(repoOwner, repoName, cb) {
-  // ...
+  // making request
+  var options = {
+    url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
+    headers: {
+      'User-Agent': 'request',
+      'Authorization': 'token ' + token.GITHUB_TOKEN // adding authorization header
+    }
+  };
+
+  request(options, function(err, res, body) {
+    cb(err, body);
+  });
 }
 
 getRepoContributors("jquery", "jquery", function(err, result) {
